4b82f5ccd6629dc76ebea87dba5bb16f79c3e521
Alexander Potashev <aspotashev@gmail.com>
2010-07-08 13:57:04 +0400

    init

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+map<string, int> db;
+
+int main()
+{
+	string input;
+
+	ifstream fin("input");
+	while (!fin.eof())
+	{
+		string tmp;
+		fin >> tmp;
+		input += tmp;
+	}
+	fin.close();
+
+	cout << "Input size: " << input.size() / 1000000.0 << " MB" << endl;
+
+	for (int i = 0; i < 100000 /*((int)input.size() - 1)*/; i ++)
+	{
+		string s = input.substr(i, 20);
+		db[s] = i;
+	}
+
+	map<string, int>::iterator iter = db.lower_bound(string("Hello"));
+	for (int i = 0; i < 5; i ++, iter ++)
+	{
+		cout << "key: " << iter->first << endl;
+	}
+
+	return 0;
+}
+
