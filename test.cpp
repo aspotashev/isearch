ef611514c6064f47e254664cc6b4da0bddefa361
Alexander Potashev <aspotashev@gmail.com>
2010-07-08 20:11:41 +0400

    cleanup

--- test.cpp
+++ test.cpp
@@ -23,11 +23,12 @@ int main()
 
 	cout << "Input size: " << input.size() / 1000000.0 << " MB" << endl;
 
-	for (int i = 0; i < 100000 /*((int)input.size() - 1)*/; i ++)
+	for (int i = 0; i < (int)input.size() - 1; i ++)
 	{
 		string s = input.substr(i, 20);
 		db.push_back(make_pair(s, i));
 	}
+
 
 //	map<string, int>::iterator iter = db.lower_bound(string("Hello"));
 //	for (int i = 0; i < 5; i ++, iter ++)
