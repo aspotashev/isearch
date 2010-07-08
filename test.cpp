65162b76779cca593c4349578791f9d9a9afe1e9
Alexander Potashev <aspotashev@gmail.com>
2010-07-08 22:28:41 +0400

    search in vector (stub)

--- test.cpp
+++ test.cpp
@@ -3,10 +3,18 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<pair<string, int> > db;
+vector<int> db_i;
+
+bool myfunction(int i, int j)
+{
+	cout << "i = " << i << ", j = " << j << endl;
+	return true;
+}
 
 int main()
 {
@@ -23,12 +31,23 @@ int main()
 
 	cout << "Input size: " << input.size() / 1000000.0 << " MB" << endl;
 
-	for (int i = 0; i < (int)input.size() - 1; i ++)
+	for (int i = 0; i < 1*1000*1000 /*(int)input.size()*/; i ++)
 	{
 		string s = input.substr(i, 20);
 		db.push_back(make_pair(s, i));
 	}
 
+	sort(db.begin(), db.end());
+	for (int i = 0; i < (int)db.size(); i ++)
+	{
+		db_i.push_back(db[i].second);
+	}
+
+	vector<int>::iterator iter = lower_bound(db_i.begin(), db_i.end(), -1, myfunction);
+	for (int i = 0; i < 5; i ++, iter ++)
+	{
+		cout << "res: " << *iter << "      ---> " << input.substr(*iter, 20) << endl;
+	}
 
 //	map<string, int>::iterator iter = db.lower_bound(string("Hello"));
 //	for (int i = 0; i < 5; i ++, iter ++)
