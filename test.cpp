c197cf727d4072840470437338c3461aea79b019
Alexander Potashev <aspotashev@gmail.com>
2010-08-03 16:53:17 +0400

    cleanup

--- test.cpp
+++ test.cpp
@@ -5,11 +5,6 @@ int main()
 	init_search("../dump.dat", "../dump-map.txt");
 
 
-//	for (int i = 0; i < input_sz; i ++)
-//	{
-//		print_unicode(input + db[i], 20);
-//	}
-
 	printf("start...\n");
 	for (int i = 0; i < 1000 * 1000; i ++)
 	{
