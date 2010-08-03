7b0ecfeead20a4a6c19fdaa03dc7a20ee65842d1
Alexander Potashev <aspotashev@gmail.com>
2010-08-03 16:27:10 +0400

    cleanup

--- test.cpp
+++ test.cpp
@@ -1,35 +1,5 @@
 #include "lib.h"
 
-using namespace std;
-
-void print_unicode(void *s, int chars)
-{
-	char buf[200];
-
-	size_t inbytesleft = 2*chars;
-	size_t outbytesleft = 200;
-	char *input_part = (char *)s;
-	char *output_part = buf;
-
-	iconv_t cd = iconv_open("UTF-8", "UCS-2");
-	iconv(cd, &input_part, &inbytesleft, &output_part, &outbytesleft);
-	iconv_close(cd);
-
-	*output_part = '\0';
-
-	printf("TEXT: %s", buf);
-	printf("     ");
-	for (int i = 0; i < 10; i ++)
-	{
-		printf("%04x ", ((char_t *)s)[i]);
-	}
-
-	int index = (char_t *)s - input;
-	printf("     %s", index_to_string_id(index));
-
-	printf("\n");
-}
-
 int main()
 {
 	init_search("../dump.dat", "../dump-map.txt");
